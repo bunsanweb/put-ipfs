f168e0209b55198af43e8a3ddf9563fde6cdb92d
bellbind <bellbind@gmail.com>
2019-11-25 17:19:32 +0900

    [init]

---
+++ modules/put-ipfs.js
@@ -0,0 +1,64 @@
+
+// option defaults
+const gateway = options => {
+  return options.gateway || "https://gateway.ipfs.io/ipfs";
+};
+const fetchImpl = options => {
+  return typeof options.fetch === "function" ? options.fetch : fetch;
+};
+const trial = options => {
+  return Number.isInteger(options.trial) ?
+    Math.max(1, Math.min(options.trial, 10)) : 10;
+};
+const checkReached = options => Boolean(options.checkReached);
+
+// put bundle
+export const put = async (node, bundle, options = {}) => {
+  // bundle as: {"index.html": "<body>Hello</body>", [path1]: data1, ...}
+  await node.ready;
+  await cleanupMFS(node);
+  try {
+    const {Buffer} = node.constructor;
+    for (const [path, content] of Object.entries(bundle)) {
+      if (!path) continue;
+      await node.files.write(
+        `/${path}`, Buffer.from(content), {create: true});
+    }
+    await node.files.flush("/");
+    const root = await node.files.stat("/");
+    const base = `${gateway(options)}/${root.hash}/`;
+    const pinset = await node.pin.add(root.hash);
+    if (checkReached(options)) {
+      console.debug("await reached to gateway:", base);
+      await waitAccessible(base, Object.keys(bundle), options);
+    }
+    return base;
+  } finally {
+    await cleanupMFS(node);    
+  }
+};
+
+const cleanupMFS = async node => {
+  for (const {name} of await node.files.ls("/")) {
+    await node.files.rm(`/${name}`, {recursive: true});
+  }
+};
+
+export const waitAccessible = async (base, pathList, options) => {
+  outer: for (const path of pathList) {
+    const url = `${base}${path}`;
+    let timeout = 100;
+    for (let count = 0; count < trial(options); count++) {
+      try {
+        await new Promise(f => setTimeout(f, timeout * (1 + Math.random())));
+        timeout *= 2;
+        console.debug("await to fetch:", url);
+        const res = await fetch(url, {method: "HEAD"});
+        if (res.ok) continue outer;
+        console.debug(`status of fetch ${url}:`, res.status);
+      } catch (error) {
+        console.debug(`error when fetch ${url}:`, error);
+      }
+    }
+  }
+};
---
+++ test/put-html/main.js
@@ -0,0 +1,32 @@
+import * as PutIpfs from "./modules/put-ipfs.js";
+import "https://cdn.jsdelivr.net/npm/ipfs/dist/index.js";
+//console.log(window.Ipfs);
+
+const main = async () => {
+  console.log("[INFO] IPFS node spawn several logs includes WebSocket Errors");
+  const node = new Ipfs({
+    repo: `ipfs-${Math.random()}`,
+    relay: {enabled: true, hop: {enabled: true, active: true}},
+  });
+  await node.ready;
+  //console.log("IPFS version:", (await node.version()).version);
+  //console.log(`Peer ID:`, (await node.id()).id);
+
+  const bundle = {
+    "index.html": `
+<html>
+<head>
+<script type="module" src="./main.js"></script>
+</head>
+<body>
+</body>
+</html>`,
+    "main.js": `document.body.append("Hello World");`,
+  };
+  const url = await PutIpfs.put(node, bundle, {checkReached: true});
+  
+  console.log("[published url]", url);
+  
+  window.finish();
+};
+main().catch(err => console.error(err.message));
