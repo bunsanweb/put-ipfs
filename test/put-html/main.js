d09f9ce50a495c2a70bef3a12219fe5ba856098d
bellbind <bellbind@gmail.com>
2020-01-23 17:35:46 +0900

    [update] extract test runner to test-on-puppeteer package
    - see: https://github.com/bunsanweb/test-on-puppeteer

--- test/put-html/main.js
+++ test/put-html/main.js
@@ -1,17 +1,17 @@
 import * as PutIpfs from "http://localhost:10000/put-ipfs.js";
 import "https://cdn.jsdelivr.net/npm/ipfs/dist/index.js";
-//console.log(window.Ipfs);
+//console.debug(window.Ipfs);
 
 const main = async () => {
-  console.log("[INFO] IPFS node spawn several logs includes WebSocket Errors");
+  console.info("[INFO] IPFS node spawn several logs includes WebSocket Errors");
   const node = new Ipfs({
     repo: `ipfs-${Math.random()}`,
     relay: {enabled: true, hop: {enabled: true, active: true}},
   });
   await node.ready;
   window.ipfsNode = node;
-  //console.log("IPFS version:", (await node.version()).version);
-  //console.log(`Peer ID:`, (await node.id()).id);
+  //console.debug("IPFS version:", (await node.version()).version);
+  //console.debug(`Peer ID:`, (await node.id()).id);
 
   const bundle = {
     "index.html": `
@@ -30,7 +30,7 @@ const main = async () => {
     waitWithGet: true,
   });
   
-  console.log("[published url]", url);
+  console.info("[published url]", url);
   
   if (typeof window.finish === "function") window.finish();
 };
